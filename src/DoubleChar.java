4fbb2fd55cec3cb83121de8ba45c28309589ab2f
Kitekat196 <gitler.kaka@mail.ru>
2023-08-18 19:32:47 +0300

    codewars

---
+++ src/DoubleChar.java
@@ -0,0 +1,17 @@
+public class DoubleChar {
+    public static void main(String[] args) {
+        System.out.println(solution("String"));
+    }
+
+
+    public static String solution(String str){
+        String newString = "";
+        for (int i = 0; i < str.length(); i++) {
+            char buff = str.charAt(i);
+            newString = newString + buff + "" + buff;
+        }
+        return newString;
+
+    }
+}
+//str.replaceAll(".", "$0$0");
