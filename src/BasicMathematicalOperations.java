771a92926fb85373c0843c875ebcd5a2af4f5404
Kitekat196 <gitler.kaka@mail.ru>
2023-08-21 11:32:43 +0300

    codewars

---
+++ src/BasicMathematicalOperations.java
@@ -0,0 +1,14 @@
+public class BasicMathematicalOperations {
+    public static Integer basicMath(String op, int v1, int v2) {
+        return switch(op) {
+            case "+" -> v1 + v2;
+            case "-" -> v1 - v2;
+            case "*" -> v1 * v2;
+            case "/" -> v1 / v2;
+            default -> null;
+        };
+    }
+}
+/*
+
+ */
